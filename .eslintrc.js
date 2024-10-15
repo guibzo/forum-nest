521df77756e67b4250f06fef1eb6692e70b9f18a
Guilherme Viana <imluizguilara@hotmail.com>
2024-10-15 08:55:13 -0300

    feat: setup docker & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['plugin:@darraghor/nestjs-typed/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+    ecmaVersion: 'es2019',
+  },
+  plugins: ['@darraghor/nestjs-typed'],
+}
